6687f080cf698811e0e8b50ffa89b3de98be3d3d
kekefreedog <kevin.zarshenas@gmail.com>
2021-09-29 15:02:35 +0200

    Update Google.php

--- php/Google.php
+++ php/Google.php
@@ -87,6 +87,10 @@ class Google {
 
         }
 
+        print_r($accessToken);
+        print_r('//////');
+        print_r($this->client->isAccessTokenExpired());
+
         // If there is no previous token or it's expired.
         if ($this->client->isAccessTokenExpired()) {
 
