8cb5ae493922783878f0e4aea7df151992597111
kekefreedog <kevin.zarshenas@gmail.com>
2022-07-22 11:59:57 +0200

    Add format support for some languages

--- resources/js/bundle.js
+++ resources/js/bundle.js
@@ -17,3 +17,35 @@ require("Kmaterialize/src/js/search.js");
 
 /* Import Prism */
 require('prismjs');
+require('prismjs/components/prism-c');
+require('prismjs/components/prism-cpp');
+require('prismjs/components/prism-docker');
+require('prismjs/components/prism-git');
+require('prismjs/components/prism-php');
+require('prismjs/components/prism-json');
+require('prismjs/components/prism-mel');
+require('prismjs/components/prism-yaml');
+require('prismjs/components/prism-sql');
+require('prismjs/components/prism-regex');
+require('prismjs/components/prism-python');
+require('prismjs/components/prism-markdown');
+require('prismjs/components/prism-markup-templating');
+require('prismjs/components/prism-mongodb');
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-css');
+require('prismjs/components/prism-xml-doc');
+require('prismjs/components/');
+const loadLanguages = require('prismjs/components/');
+window.Prism.manual = true;
+// THE FOLLOWING BLOCK CHANGED:
+jQuery(function () {
+    // Wrap the code inside the required <code> tag, when needed:
+    jQuery('pre[class*="language-"], pre[class*="lang-"]').each(function () {
+        if (1 !== jQuery(this).children('code').length) {
+            jQuery(this).wrapInner('<code>');
+        }
+    });
+
+    // Highlight code, when the page finished loading (using jQuery here)
+    Prism.highlightAll()
+});
